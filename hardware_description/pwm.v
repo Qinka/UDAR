efea6d55f07199620f38a0354a11bf14a63b416d
Johann Lee <me@qinka.pro>
2018-05-07 20:28:36 +0800

    add files; update pwm.v
    Add the there files:
        1. ctrl.v for controlling hold system
        2. pwm.v for Pulse Widh Modulation
        3. udd.v for ultrasonic rangefinder device driver
    
    Update the pwm.v:
        1. finish the pwm module with the example from embeddedmicro's Mojo v3's tutorial.

---
+++ hardware_description/pwm.v
@@ -0,0 +1,44 @@
+/**
+ * This module is a part of UDAR under GPLv3
+ * Copyright (C) 2018 Johann Lee
+ *
+ * Pulse Width Modulation
+ */
+
+
+// The pwn module
+module pwm #(parameter CTR_LEN = 8)
+   ( input clk, // clock signal
+     input rst, // reset signal
+     input [CTR_LEN - 1 : 0] cmp_reg, // the reigster of compare
+     output sig // output signal
+    );
+
+   // signal reg
+   reg      pwm_d, pwm_q;
+   // counter
+   reg      ctr_d, ctr_q;
+
+   assign pwm = pwm_q;
+
+   // compare
+   always @(*) begin
+      ctr_d = ctr_q + 1'b1;
+
+      if (cmp_reg > ctr_q)
+        pwm_d = 1'b1;
+      else
+        pwm_d = 1'b0;
+   end
+
+   // clock
+   always @(posedge clk) begin
+      if (rst)
+        ctr_q <= 1'b0;
+      else
+        ctr_q <= ctr_d;
+
+      pwm_q <= pwm_d;
+   end
+
+endmodule
